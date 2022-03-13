bacc708f2eb17be0a7ebbb3433683411e42e1631
chuf <907701820@qq.com>
2022-03-13 17:05:17 +0800

    会员验证码

--- mall-member-8002/src/main/java/com/nimo/member/MemberApp8002.java
+++ mall-member-8002/src/main/java/com/nimo/member/MemberApp8002.java
@@ -2,6 +2,9 @@ package com.nimo.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @program: mall-springcloud
@@ -10,7 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: chuf
  * @create: 2022-03-13 15:48
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages={"com.nimo.common.services","com.nimo.member"})
+@EnableEurekaClient
 public class MemberApp8002 {
     public static void main(String[] args) {
         SpringApplication.run(MemberApp8002.class, args);
--- mall-member-8002/src/main/java/com/nimo/member/config/Swagger2Config.java
+++ mall-member-8002/src/main/java/com/nimo/member/config/Swagger2Config.java
@@ -1,13 +1,60 @@
-package com.nimo.member.config;/**
-  * @program: mall-springcloud
-  *
-  * @ClassName: Swagger2Config
-  *
-  * @description: 
-  *
-  * @author: chuf
-  *
-  * @create: 2022-03-13 16:46
-  **/
-public class Swagger2Config {
+package com.nimo.member.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @program: mall-springcloud
+ * @ClassName: Swagger2Config
+ * @description:
+ * @author: chuf
+ * @create: 2022-03-13 16:46
+ **/
+@Configuration
+@EnableSwagger2
+public class Swagger2Config extends WebMvcConfigurationSupport {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包下controller生成API文档
+                .apis(RequestHandlerSelectors.basePackage("com.nimo.member.controller"))
+                //为有@Api注解的Controller生成API文档
+//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SwaggerUI演示")
+                .description("mall-tiny")
+                .contact("macro")
+                .version("1.0")
+                .build();
+    }
+
+    //addResourceHandlers方法添加了两个资源处理程序，
+    //这段代码的主要作用是对Swagger UI的支持。(访问接口页面为空白时可加上)
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
 }
