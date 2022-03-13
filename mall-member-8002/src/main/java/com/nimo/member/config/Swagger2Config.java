fb3ab0257efb78a903f5ea11843778f3d7d06dd7
chuf <907701820@qq.com>
2022-03-13 17:04:35 +0800

    会员登录

---
+++ mall-member-8002/src/main/java/com/nimo/member/config/Swagger2Config.java
@@ -0,0 +1,13 @@
+package com.nimo.member.config;/**
+  * @program: mall-springcloud
+  *
+  * @ClassName: Swagger2Config
+  *
+  * @description: 
+  *
+  * @author: chuf
+  *
+  * @create: 2022-03-13 16:46
+  **/
+public class Swagger2Config {
+}
