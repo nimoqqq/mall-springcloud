0a2bbc184bb32b73c45e9a52c3792d92e89a8f2a
chuf <907701820@qq.com>
2022-03-13 16:19:47 +0800

    redis

---
+++ mall-member-8002/src/main/java/com/nimo/member/MemberApp8002.java
@@ -0,0 +1,18 @@
+package com.nimo.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: mall-springcloud
+ * @ClassName: MemberApp8002
+ * @description:
+ * @author: chuf
+ * @create: 2022-03-13 15:48
+ **/
+@SpringBootApplication
+public class MemberApp8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApp8002.class, args);
+    }
+}
---
+++ mall-member-8002/src/main/java/com/nimo/member/controller/UmsMemberController.java
@@ -0,0 +1,42 @@
+package com.nimo.member.controller;
+
+import com.nimo.common.api.CommonResult;
+import com.nimo.member.services.UmsMemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @program: mall-springcloud
+ * @ClassName: UmsMemberController
+ * @description:
+ * @author: chuf
+ * @create: 2022-03-13 15:55
+ **/
+@Controller
+@Api(tags = "UmsMemberController", description = "会员登录注册管理")
+@RequestMapping("/sso")
+public class UmsMemberController {
+    @Autowired
+    private UmsMemberService memberService;
+
+    @ApiOperation("获取验证码")
+    @RequestMapping(value = "/getAuthCode", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return memberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation("判断验证码是否正确")
+    @RequestMapping(value = "/verifyAuthCode", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updatePassword(@RequestParam String telephone,
+                                       @RequestParam String authCode) {
+        return memberService.verifyAuthCode(telephone, authCode);
+    }
+}
