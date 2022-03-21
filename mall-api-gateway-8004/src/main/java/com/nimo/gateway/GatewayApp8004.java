64dd3bbd86f2076b6f9ff48e322f93bfb1fcd35e
chuf <907701820@qq.com>
2022-03-21 16:32:03 +0800

    gateway整合 security 实现认证授权

--- mall-api-gateway-8004/src/main/java/com/nimo/gateway/GatewayApp8004.java
+++ mall-api-gateway-8004/src/main/java/com/nimo/gateway/GatewayApp8004.java
@@ -2,6 +2,7 @@ package com.nimo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author: chuf
  * @create: 2022-03-20 23:04
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 public class GatewayApp8004 {
     public static void main(String[] args) {
